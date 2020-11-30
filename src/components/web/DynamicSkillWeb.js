bc7c00ced2317d7e466f48866f9750e80cf730eb
TreHein <treheinguy@gmail.com>
2020-11-29 21:17:49 -0600

    useEffect updates on everything

--- src/components/web/DynamicSkillWeb.js
+++ src/components/web/DynamicSkillWeb.js
@@ -1,69 +1,25 @@
 // import React, { useState, useEffect } from 'react'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { ForceGraph2D } from 'react-force-graph'
 
 const DynamicSkillWeb = (props) => {
-    // const nodeData = props.graphData.nodes
-
-    // linkData = JSON.parse(JSON.stringify(props.graphData.links));
-    // nodeData = JSON.parse(JSON.stringify(props.graphData.nodes));
-
+    const cache = useRef(props.graphData);
     const [data, setData] = useState({ nodes: [], links: [] })
-
-    // Object.isExtensible(graphData)
-
     console.log(data)
-    console.log(Object.isExtensible(data));
 
-    // console.log(graphData)
 
-    // const copyNodes = (data) => {
-    //     return data.map((item) => ({
-    //         ...item
-    //     }))
-    // }
-
-    // const copyLinks = (data) => {
-    //     return data.map((item) => ({
-    //         ...item
-    //     }))
-    // }
-    
-    // var nodeCopy = copyNodes(graphData.nodes)
-    // var linkCopy = copyLinks(graphData.links)
-
-    // const dataPack = () => {
-    //     var data = {};
-    //     data.nodes = nodeCopy;
-    //     data.links = linkCopy;
-    //     return data
-    // }
-
-    // const data = dataPack()
-
-    // console.log(data)
-    var nodeData = {}
-    var linkData = {}
 
     useEffect(() => {
+        var nodeData = {}
+        var linkData = {}
         linkData = JSON.parse(JSON.stringify(props.graphData.links));
         nodeData = JSON.parse(JSON.stringify(props.graphData.nodes));
         setData({ nodes: nodeData, links: linkData })
-    }, [])
 
-    // return (
-    //     <ForceGraph2D 
-    //         graphData={graphData}
-    //         nodeLabel="id"
-    //         nodeVal={d => d.size}
-    //         nodeAutoColorBy="group"
-    //         backgroundColor="black"
-    //         linkColor="color"
-    //         linkWidth="value"
-    //         linkDirectionalParticles={d => d.value}
-    //         linkDirectionalParticleSpeed={d => d.value * 0.0005}
-    //     />
-    // )
+        cache.current = props.graphData;
+
+    }, [props.graphData.links, props.graphData.nodes, props.graphData])
+
     return (
         <ForceGraph2D 
             graphData={data}
